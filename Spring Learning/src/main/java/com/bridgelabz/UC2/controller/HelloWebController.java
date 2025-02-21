8478236c37523df3690a06ead67273ecf4a99767
HP-Harshit <harshitpatel274@gmail.com>
2025-02-21 15:44:56 +0530

    Harshit Added Solution for HelloWorld using Spring

---
+++ Spring Learning/src/main/java/com/bridgelabz/UC2/controller/HelloWebController.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.UC2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloWebController {
+    @GetMapping("/web")
+    public String hello(){
+        return "Hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message", "This is a custom message");
+        return "message";
+    }
+}
