ccd55623a81334d522fb909eae0fc84e6a509bb7
HP-Harshit <harshitpatel274@gmail.com>
2025-02-21 18:37:51 +0530

    [Harshit] Added UC2: Added SLF4J logging with logback configuration

--- springconcepts/src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ springconcepts/src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.springconcepts;
 
 import com.bridgelabz.springconcepts.component.DemoBean;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +10,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringConceptsApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
-		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
-	}
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 
+	public static void main(String[] args) {
+		logger.info("Welcome to Spring Concept Demo");
+
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		logger.debug("Application Context Initialized");
+
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		logger.debug("Checking Context: {}", demoBean);
+	}
 }
